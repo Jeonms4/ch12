4587de8daff4c42d88a5fbc5c97615183c98812a
Jeonms4 <wjsalstn100@gmail.com>
2024-11-21 23:17:37 +0900

    11_21

--- LogFilter.java
+++ LogFilter.java
@@ -1,8 +1,6 @@
 package filter;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -15,50 +13,41 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-public class LogFileFilter implements Filter {
-	PrintWriter writer;
+public class LogFilter implements Filter {
 
 	public void init(FilterConfig filterConfig) throws ServletException {
-		String filename = filterConfig.getInitParameter("filename");
-		if (filename == null)
-			throw new ServletException("로그 파일의 이름을 찾을 수 없습니다.");
-		try {
-			writer = new PrintWriter(new FileWriter(filename, true), true);
-		} catch (IOException e) {
-			throw new ServletException("로그 파일을 열 수 없습니다.");
-		}
+		System.out.println("BookMarket 초기화...");
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		writer.println(" 접속한 클라이언트 IP : " + request.getRemoteAddr());
+		System.out.println(" 접속한 클라이언트 IP : " + request.getRemoteAddr());
 		long start = System.currentTimeMillis();
-		writer.println(" 접근한 URL 경로 : " + getURLPath(request));
-		writer.println(" 요청 처리 시작 시각 : " + getCurrentTime());
-
+		System.out.println(" 접근한 URL 경로 : " + getURLPath(request));
+		System.out.println(" 요청 처리 시작 시각 : " + getCurrentTime());
 		chain.doFilter(request, response);
 
 		long end = System.currentTimeMillis();
-		writer.println(" 요청 처리 종료 시각 : " + getCurrentTime());
-		writer.println(" 요청 처리 소요 시각 : " + (end - start) + "ms ");
+		System.out.println(" 요청 처리 종료 시각 : " + getCurrentTime());
+		System.out.println(" 요청 처리 소요 시각 : " + (end - start) + "ms ");
 		System.out.println("===============================================================");
 	}
 
 	public void destroy() {
-		writer.close();
+
 	}
 
 	private String getURLPath(ServletRequest request) {
 		HttpServletRequest req;
 		String currentPath = "";
 		String queryString = "";
-		if (request instanceof HttpServletRequest) {
-			req = (HttpServletRequest) request;
+		if(request instanceof HttpServletRequest) {
+			req = (HttpServletRequest)request;
 			currentPath = req.getRequestURI();
 			queryString = req.getQueryString();
 			queryString = queryString == null ? "" : "?" + queryString;
 		}
-		return currentPath + queryString;
+		return currentPath+queryString;
 	}
 
 	private String getCurrentTime() {
--- practice04_success.jsp
+++ practice04_success.jsp
@@ -1,9 +1,9 @@
-%@ page contentType=texthtml; charset=utf-8%
-html
-head
-titleFiltertitle
-head
-body
-padmin님 로그인을 성공했습니다.
-body
-html
+<%@ page contentType="text/html; charset=utf-8"%>
+<html>
+<head>
+<title>Filter</title>
+</head>
+<body>
+<p>admin님 로그인을 성공했습니다.
+</body>
+</html>
